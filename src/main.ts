20bbe301fa370aaac52bcfd94c1a977767a8625b
Marcello-calyps <153430442+Martzcode@users.noreply.github.com>
2026-08-15 14:57:05 +0300

    Suppression menu contextuel par defaut

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import { bootstrapApplication } from "@angular/platform-browser";
 import { AppComponent } from "./app/app.component";
 import { appConfig } from "./app/app.config";
 
+document.addEventListener("contextmenu", (event) => {
+  event.preventDefault();
+});
+
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err),
 );
